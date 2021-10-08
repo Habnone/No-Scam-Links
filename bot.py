5b64c5658bc44eca6bb07fe614824190b0ba486a
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2021-10-08 17:32:56 +0300

    Change variable name
    Renamed variable "client" to "bot"

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ from discord.ext import commands, tasks
 import asyncio
 import aiohttp
 
-#Command prefix
-client = commands.Bot(command_prefix = '!')
+#set the bot variable
+bot = commands.Bot(command_prefix = '!')
 
 #Gets a list of scam domains
 async def getlinks():
@@ -16,15 +16,15 @@ async def getlinks():
             links = await resp.json()
             return links
 
-@client.event
+@bot.event
 async def on_ready():
     print("Bot is ready!")
-    client.banned_links = await getlinks()
+    bot.banned_links = await getlinks()
 
 #Checks if message has a malicious link
-@client.event
+@bot.event
 async def on_message(message):
-    if any(i in message.content for i in client.banned_links):
+    if any(i in message.content for i in bot.banned_links):
         #Deletes the message (recommended  to keep enabled)
         await message.delete()
 
@@ -34,4 +34,4 @@ async def on_message(message):
         #Bans the member from your server (remove # to enable)
         #await message.author.ban(reason='malicious link', delete_message_days=1)
 
-client.run('BOT TOKEN HERE')
+bot.run('BOT TOKEN HERE')
