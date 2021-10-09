59b0c1874b6c8a772b166afbaf32e2ad134ea1cc
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2021-10-09 11:59:31 +0300

    Update bot.py
    Added more Exception handlers and fixed some punctuation mistakes

--- bot.py
+++ bot.py
@@ -20,8 +20,12 @@ async def getlinks():
 async def del_message(msg):
     try:
         await msg.delete()
-    except discord.HTTPException:
-        print('Failed deleting message')
+    except discord.HTTPException(response, message):
+        print(f'Failed deleting message with ID : {message.id} for the second time')
+    except discord.NotFound(response, message):
+        print(f'The message with ID : {message.id} was already deleted')
+    else:
+        print('Second attempt to delete message with ID: {msg.id} was succesful')
 
 @bot.event
 async def on_ready():
@@ -36,18 +40,20 @@ async def on_message(message):
         #Deletes the message and print a console message if it fails (recommended  to keep enabled)
         try:
             await message.delete()
-        except discord.Forbidden:
-            print("I don't have the proper permissions to delete the message")
-        except discord.NotFound:
-            print('This message was already deleted by someone else')
-        except discord.HTTPException:
-            print('An error occured while tried deleting that message. I will retry 1 time')
+        except discord.Forbidden(response, msg):
+            print(f"I don't have the proper permissions to delete the message with ID : {msg.id}")
+        except discord.NotFound(response, msg):
+            print(f'The message with ID : {msg.id} was already deleted by someone else')
+        except discord.HTTPException(response, msg):
+            print(f'An error occured while tried deleting the messagewith ID : {msg.id}. I will retry 1 time')
             await del_message(message)
+        else:
+            print(f'The message with ID : {message.id} was succesfully deleted')
 
         #Kicks the member from your server (remove # to enable)
-        #await message.author.kick(reason="malicious link")
+        #await message.author.kick(reason="Malicious link")
 
         #Bans the member from your server (remove # to enable)
-        #await message.author.ban(reason='malicious link', delete_message_days=1)
+        #await message.author.ban(reason='Malicious link', delete_message_days=1)
 
 bot.run('BOT TOKEN HERE')
