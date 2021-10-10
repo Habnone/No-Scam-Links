488888241fd0cf518653d4e827c00de27e7dde81
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2021-10-10 15:03:32 +0300

    Update bot.py
    Added 2 features:
    
    1) Each time something occurs (bot login, message deletion, etc.) the current time is printed in the console
    
    2) If the token file still has the default text, then the program doesn't attemp to login to discord at all and exits

--- bot.py
+++ bot.py
@@ -2,40 +2,46 @@ import discord
 from discord.ext import commands, tasks
 import asyncio
 import aiohttp
+import sys
+from datetime import datetime
 
 #set the bot variable
 bot = commands.Bot(command_prefix = '')
 
+async def get_current_time():
+    now = datetime.now()
+    return now.strftime("%H:%M:%S")
+
 #Gets a list of scam domains
 @tasks.loop(seconds=300.0)
 async def getlinks():
-
+    print(f'{await get_current_time()} | Fetching scam links list...')
+    
     async with aiohttp.ClientSession() as session:
 
         banned_links = 'https://api.hyperphish.com/gimme-domains'
         async with session.get(banned_links) as resp:
             links = await resp.json()
-            print('Got scam links data. Dumping them into a variable...')
+            print(f'{await get_current_time()} | Got scam links data. Dumping them into a variable...')
             bot.banned_links = links
-            print('Dumbing done')
+            print(f'{await get_current_time()} | Dumbing done')
 
 #Deletes a message:
 async def del_message(msg):
     try:
         await msg.delete()
     except discord.HTTPException(response, message):
-        print(f'Failed deleting message with ID : {message.id} for the second time')
+        print(f'{await get_current_time()} | Failed deleting message with ID : {message.id} for the second time')
     except discord.NotFound(response, message):
-        print(f'The message with ID : {message.id} was already deleted')
+        print(f'{await get_current_time()} | The message with ID : {message.id} was already deleted')
     else:
-        print('Second attempt to delete message with ID: {msg.id} was succesful')
+        print(f'{await get_current_time()} | Second attempt to delete message with ID: {msg.id} was succesful')
 
 @bot.event
 async def on_ready():
-    print('Fetching scam links list...')
     getlinks.start()
     #bot.banned_links = await getlinks()
-    print("Bot is ready!")
+    print(f"{await get_current_time()} | Bot is ready!")
 
 # Checks if message has a malicious link
 @bot.event
@@ -45,14 +51,14 @@ async def on_message(message):
         try:
             await message.delete()
         except discord.Forbidden(response, msg):
-            print(f"I don't have the proper permissions to delete the message with ID : {msg.id}")
+            print(f"{await get_current_time()} | I don't have the proper permissions to delete the message with ID : {msg.id}")
         except discord.NotFound(response, msg):
-            print(f'The message with ID : {msg.id} was already deleted by someone else')
+            print(f'{await get_current_time()} | The message with ID : {msg.id} was already deleted by someone else')
         except discord.HTTPException(response, msg):
-            print(f'An error occured while tried deleting the message with ID : {msg.id}. I will retry 1 time')
+            print(f'{await get_current_time()} | An error occured while tried deleting the message with ID : {msg.id}. I will retry 1 time')
             await del_message(message)
         else:
-            print(f'The message with ID : {message.id} was succesfully deleted')
+            print(f'{await get_current_time()} | The message with ID : {message.id} was succesfully deleted')
 
         #Kicks the member from your server (remove # to enable)
         #await message.author.kick(reason="Malicious link")
@@ -62,7 +68,12 @@ async def on_message(message):
 
 # load the bot token from the "token" file
 with open('token') as f:
-    bot_token = f.read()
+    if f.read() == 'ENTER BOT TOKEN HERE':
+        sys.exit('No token found in file "token"')
+    else:
+        f.seek(0)
+        bot_token = f.read()
+
 
 # run the bot
 bot.run(bot_token)
